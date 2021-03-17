95d5438d49a01d01e47592dc5fd436fc3c6d85b2
Josh Malek <17jmalek98@gmail.com>
2021-03-17 15:23:25 -0400

    add search page with drawer

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:mobile_fl/API/GQLConfig.dart';
 import 'package:mobile_fl/components/AuthWrapper.dart';
 import 'package:mobile_fl/screens/Login.dart';
 import 'package:mobile_fl/screens/Register.dart';
+import 'package:mobile_fl/screens/Search.dart';
 import 'package:hive/hive.dart';
 
 // Setup GraphQL Client
@@ -103,27 +104,6 @@ class _LandingScreenState extends State<LandingScreen> {
             )
           ],
         ),
-      ),
-    );
-  }
-}
-
-class SearchScreen extends StatefulWidget {
-  @override
-  _SearchScreenState createState() => _SearchScreenState();
-}
-
-class _SearchScreenState extends State<SearchScreen> {
-  @override
-  Widget build(BuildContext ctx) {
-    return AuthWrapper(
-      authLevel: AuthLevels.STUDENT,
-      ctx: ctx,
-      body: Scaffold(
-        appBar: AppBar(
-          title: Text("Search"),
-        ),
-        body: Text("Search Page"),
       ),
     );
   }
---
+++ lib/screens/Search.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:mobile_fl/API/AuthAPI.dart';
+import 'package:mobile_fl/API/GQLConfig.dart';
+import 'package:mobile_fl/components/AuthWrapper.dart';
+import 'package:mobile_fl/screens/Login.dart';
+import 'package:mobile_fl/screens/Register.dart';
+import 'package:hive/hive.dart';
+
+class SearchScreen extends StatefulWidget {
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  @override
+  Widget build(BuildContext ctx) {
+    return AuthWrapper(
+      authLevel: AuthLevels.STUDENT,
+      ctx: ctx,
+      body: Scaffold(
+        appBar: AppBar(
+          title: Text("Search"),
+        ),
+        body: Text("Search Page"),
+        drawer: Drawer(
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Drawer Header'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text('Item 1'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              ListTile(
+                title: Text('Item 2'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

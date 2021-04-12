679ba4245a2c112eea8b5cd5dccf55826796e8e6
Abdul-Muiz Yusuff <58189969+sacrael@users.noreply.github.com>
2021-04-11 22:46:12 -0400

    Partially completed new property card component

--- lib/screens/Search.dart
+++ lib/screens/Search.dart
@@ -47,15 +47,150 @@ class _SearchScreenState extends State<SearchScreen> {
 
   @override
   Widget build(BuildContext ctx) {
-    return Container(
-        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
-        child: Column(children: [
-          Expanded(
-              flex: 1,
-              child: Column(
-                children: [
-                  Text("Results"),
-                  Query(
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.centerLeft,
+          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+          child: Row(
+            children: [
+              Text("Filter"),
+              Text(
+                "3 Filters Applied",
+                style: TextStyle(color: Constants.pink(), fontSize: 12),
+              )
+            ],
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          ),
+          decoration: BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(
+                      color: Constants.navy(opacity: 0.3), width: 1))),
+        ),
+        SingleChildScrollView(
+            child: Container(
+          alignment: Alignment.topLeft,
+          child: Column(
+            children: [NewPropertyCard(), NewPropertyCard()],
+          ),
+        ))
+      ],
+    );
+  }
+}
+
+class NewPropertyCard extends StatefulWidget {
+  @override
+  _NewPropertyCardState createState() => _NewPropertyCardState();
+}
+
+class _NewPropertyCardState extends State<NewPropertyCard> {
+  @override
+  Widget build(BuildContext ctx) => Container(
+        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Stack(
+          children: [
+            Positioned(
+                child: Text(
+                  "\$300-600 /month",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+                right: 10,
+                top: 0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Text 1 Sample",
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      Text("Troy NY, 12180")
+                    ],
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                    child: Row(children: (() {
+                      List<String> tagStrs = ["Furnished", "Washer", "Heating"];
+                      List<Widget> tags = [];
+
+                      for (int i = 0; i < tagStrs.length; ++i) {
+                        tags.add(Container(
+                          child:
+                              Text(tagStrs[i], style: TextStyle(fontSize: 10)),
+                          padding: EdgeInsets.fromLTRB(6, 4, 6, 4),
+                          margin: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                          decoration: BoxDecoration(
+                              color: Constants.navy(opacity: 0.12),
+                              borderRadius: BorderRadius.circular(3)),
+                        ));
+                      }
+
+                      return tags;
+                    })())),
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  decoration: BoxDecoration(border: Border.all(width: 1)),
+                  constraints: BoxConstraints.expand(height: 100),
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      Container(
+                          color: Colors.red,
+                          constraints: BoxConstraints.tightFor(
+                            width: 200,
+                            height: 100,
+                          )),
+                      Container(
+                          color: Colors.green,
+                          constraints: BoxConstraints.tightFor(
+                            width: 200,
+                            height: 100,
+                          )),
+                      Container(
+                          color: Colors.blue,
+                          constraints: BoxConstraints.tightFor(
+                            width: 200,
+                            height: 100,
+                          ))
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Text("Roe Jogan / 10 Photos / 4 Reviews",
+                      style: TextStyle(fontSize: 12)),
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        color: Colors.red,
+                        child: Text("Landlord Rating Goes Here"),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        color: Colors.orange,
+                        child: Text("Property Rating Goes Here"),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      );
+}
+/*
+Query(
                     options: QueryOptions(
                         document: gql(PropertyQuery.searchForPropertyGQL()),
                         variables: {
@@ -90,8 +225,4 @@ class _SearchScreenState extends State<SearchScreen> {
                       ));
                     },
                   )
-                ],
-              )),
-        ]));
-  }
-}
+                  */
